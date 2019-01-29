7357cbaf26919addbc8c8a919f58cecf2093ff19
sunbo <qiongxiaozi158@sina.com>
2019-01-29 22:38:19 +0000

    Q20

---
+++ LeetCode/Q0020/Kie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Kie
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach(var i in s)
+            {
+                if ("([{".Contains(i)) stack.Push(i);
+                else
+                {
+                    if (stack.Count == 0) return false;
+                    switch (i)
+                    {
+                        case ')':
+                            if (stack.Pop() != '(') return false;
+                            break;
+                        case ']':
+                            if (stack.Pop() != '[') return false;
+                            break;
+                        case '}':
+                            if (stack.Pop() != '{') return false;
+                            break;
+                    }
+                }
+            }
+            return stack.Count==0;
+        }
+
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.IsValid(""));
+        }
+    }
+}
