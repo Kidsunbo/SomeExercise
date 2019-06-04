2960bbd59ba645a3843d00d378e20dfad8eb0e8d
Bo Sun (MSc Computer Science FT) <qiongxiaozi158@sina.com>
2019-06-04 19:32:46 +0100

    update the readme

--- LeetCode/Q0039/Kie.js
+++ LeetCode/Q0039/Kie.js
@@ -1,32 +1,9 @@
 /**
- * @param {number[]} nums
+ * @param {number[]} candidates
  * @param {number} target
- * @return {number[]}
+ * @return {number[][]}
  */
-var searchRange = function(nums, target) {
-    if(nums.length===0) return [-1,-1];
-    if(nums.length===1) return nums[0]===target?[0,0]:[-1,-1];
-    let beg = 0;
-    let end = nums.length-1;
-    let mid;
-
-    while(beg<=end){
-        mid = Math.floor((beg+end)/2);
-        if(nums[mid]>target) end = mid-1;
-        else if(nums[mid]<target) beg = mid+1;
-        else break;
-    }
-
-    if(beg>end) return [-1,-1];
-    //Determine beg
-    let beg_temp = mid;
-    while(nums[--beg_temp]===target);
-    beg = beg_temp+1;
-    //Determine end
-    let end_temp = mid;
-    while(nums[++end_temp]===target);
-    end = end_temp-1;
-    return [beg,end];
+var combinationSum = function(candidates, target) {
 
 };
 
@@ -34,7 +11,7 @@ window.onload=function () {
     $("#param").keyup(function () {
         let values = $("#param").val().split(",").map(x=>parseInt(x));
         if(values[1]&&values[1]!=="") {
-            $("#output").text(searchRange(values.slice(0,values.length-1),values[values.length-1]));
+            $("#output").text();
         }
 
     })
--- LeetCode/Q0039/README.md
+++ LeetCode/Q0039/README.md
@@ -1,18 +1,30 @@
-# 34. Find First and Last Position of Element in Sorted Array
+# 39. Combination Sum
 
-Given an array of integers `nums` sorted in ascending order, find the starting and ending position of a given target value.
+Given a **set** of candidate numbers (`candidates`) **(without duplicates)** and a target number (`target`), find all unique combinations in `candidates` where the candidate numbers sums to `target`.
 
-Your algorithm's runtime complexity must be in the order of *O(log n)*.
+The **same** repeated number may be chosen from `candidates` unlimited number of times.
 
-If the `target` is not found in the array, return `[-1, -1]`.
+**Note:**
+
+- All numbers (including target) will be positive integers.
+- The solution set must not contain duplicate combinations.
 
 Example 1:
 ```
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
 ```
 Example 2:
 ```
-Input: nums = [5,7,7,8,8,10], target = 6
-Output: [-1,-1]
+Input: candidates = [2,3,5], target = 8,
+A solution set is:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
 ```
