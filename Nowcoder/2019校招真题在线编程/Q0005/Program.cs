9c6d4a361d601a9e961f340000ab2b626fa1399e
Kie <qiongxiaozi158@sina.com>
2019-09-20 23:21:28 +0800

    Q5

---
+++ Nowcoder/2019校招真题在线编程/Q0005/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> input = Console.ReadLine().Split(' ').Select(int.Parse);
+            int n = input.First();
+            int k = input.Last();
+            decimal count = 0m;
+            for (int i = k+1; i <= n; i++)
+            {
+                int quotient = n / i;
+                int reminder = i-k;
+                count += quotient * reminder;
+                if (k == 0) count--;// The number is between 1 and n, so if k is 0, divident can't be zero
+                count += Math.Max(n % i - k + 1, 0);
+            }
+            Console.WriteLine(count);
+            Console.Read();
+        }
+    }
+}
---
+++ Nowcoder/2019校招真题在线编程/Q0005/README.md
@@ -0,0 +1,3 @@
+# 数对
+
+[link](https://www.nowcoder.com/practice/bac5a2372e204b2ab04cc437db76dc4f?tpId=98&tqId=32828&tPage=1&rp=1&ru=%2Fta%2F2019test&qru=%2Fta%2F2019test%2Fquestion-ranking)
