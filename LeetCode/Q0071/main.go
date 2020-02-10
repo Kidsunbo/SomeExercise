cb6eadb6a634c8c9dc633988a7a521c4ca5cebdd
Kie <qiongxiaozi158@sina.com>
2020-02-10 12:33:42 +0800

    Q71

---
+++ LeetCode/Q0071/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type test struct {
+	a int
+	b int
+}
+
+
+func simplifyPath(path string) string {
+	_paths :=strings.Split(path,"/")
+	var paths []string
+	for _,i := range _paths{
+		if i==""||i=="."{
+			continue
+		}
+		paths = append(paths,i)
+
+	}
+
+	var result []string
+	for _,i :=range paths{
+		if i==".." {
+			if len(result) >= 1 {
+				result = result[:len(result)-1]
+			}
+		} else{
+			result = append(result,i)
+		}
+	}
+
+
+	return "/"+strings.Join(result,"/")
+}
+
+
+func main(){
+
+	fmt.Println(simplifyPath("//.."))
+
+
+}
+
+
